c93d014e4c2f0024a6ee8454100e842a6cec7e1a
unknown <nobelleon.86@gmail.com>
2025-07-09 06:41:50 +0700

    adding sound to art webster

--- artwebster.js
+++ artwebster.js
@@ -1030,3 +1030,30 @@ var cursor = {
 cursor.init();
 
 
+// ------------------------------------------------------- //
+// -----------------------  SOUND  ----------------------- //  
+
+
+const sounds = ["sound"];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+
+  btn.innerText = sound;
+
+  btn.addEventListener("click", () => {
+    stopSounds();
+    document.getElementById(sound).play();
+  });
+
+  document.getElementById("buttons").appendChild(btn);
+});
+
+function stopSounds() {
+  sounds.forEach((sound) => {
+    const s = document.getElementById(sound);
+    s.pause();
+    s.currentTime = 0;
+  });
+}
