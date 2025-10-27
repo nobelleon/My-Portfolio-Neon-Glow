88248e95d3a0428d0f53cd9d02b7fc72d39cb786
unknown <nobelleon.86@gmail.com>
2025-10-27 21:01:46 +0700

    adding sound to round abstract art

--- round_abstract_art.js
+++ round_abstract_art.js
@@ -355,3 +355,31 @@ var cursor = {
 
 cursor.init();
 
+
+// ------------------------------------------------------- //
+// -----------------------  SOUND  ----------------------- //  
+
+
+const sounds = ["sound"];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+
+  btn.innerText = sound;
+
+  btn.addEventListener("click", () => {
+    stopSounds();
+    document.getElementById(sound).play();
+  });
+
+  document.getElementById("buttons").appendChild(btn);
+});
+
+function stopSounds() {
+  sounds.forEach((sound) => {
+    const s = document.getElementById(sound);
+    s.pause();
+    s.currentTime = 0;
+  });
+}
